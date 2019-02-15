02f8c2f378f4e02a1d399e08ea93396c9cb1b6de
seandunn <seantd@live.com>
2019-02-15 12:11:39 -0800

    Fixed ECR REPO

--- update-image.sh
+++ update-image.sh
@@ -2,8 +2,7 @@
 
 LOGICAL_NAME=${GITHUB_REPOSITORY/repostnetwork\//}
 
-AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query 'Account')
-ECR_REPOSITORY=`aws ecr get-login --no-include-email --region us-east-1 | awk 'NF{ print $NF }'`
+ECR_REPOSITORY=`aws ecr get-login --no-include-email --region us-east-1 | awk 'NF{ print $NF }' | sed -e 's/^http:\/\///g' -e 's/^https:\/\///g'`
 ECR_URI=$ECR_REPOSITORY/$LOGICAL_NAME
 eval $(aws ecr get-login --no-include-email --region us-east-1)
 
@@ -11,19 +10,14 @@ echo $LOGICAL_NAME
 echo $ECR_REPOSITORY
 echo $ECR_URI
 
-# Configure ECR
-echo -e "\n\nCreating ECR repository..."
+echo -e "Creating ECR repository..."
 aws ecr create-repository --repository-name $LOGICAL_NAME || true
 
-# Building Docker
-echo -e "\n\nBuilding docker..."
-docker build -t $LOGICAL_NAME --build-arg ENV=$ENV .
+echo -e "Building Docker..."
+docker build -t $LOGICAL_NAME --build-arg ENV=$ENV . > /dev/null
 
-# Tag Image
+echo -e "Tagging Image..."
 docker tag $LOGICAL_NAME:latest $ECR_URI:latest
 
-echo -e "\n\Pushing to ECR..."
+echo -e "Pushing to ECR..."
 docker push $ECR_URI:latest
-
-# Force a new deployment for the service
-aws ecs update-service --force-new-deployment --cluster repost --service $LOGICAL_NAME
